d0f185441ea64a19b1eb6f59fa4c58a910d4236e
Andrew Jye Shih Chuang <achuang@cpqd.com.br>
2018-08-14 13:43:11 -0300

    Ready for iotmid04

--- kubernetes/kill_pods.sh
+++ kubernetes/kill_pods.sh
@@ -3,4 +3,3 @@ kubectl delete -f mongocfg.yaml
 kubectl delete -f mongosh0.yaml
 kubectl delete -f mongosh1.yaml
 kubectl delete -f mongosh2.yaml
-kubectl delete namespace dojot
--- kubernetes/start_pods.sh
+++ kubernetes/start_pods.sh
@@ -10,27 +10,27 @@ echo 'starting config server replica'
 kubectl -n dojot apply -f mongocfg.yaml
 
 echo 'waiting for initialization to finish'
-sleep 2m
+sleep 45
 
 echo 'starting mongos'
 export CONFIG_ENDPOINTS=$(kubectl -n dojot get endpoints mongocfg | grep mongocfg | awk '{print $2}')
 sed -i '/configReplSet/c\        - configReplSet/'"$CONFIG_ENDPOINTS" mongos.yaml
 kubectl -n dojot apply -f mongos.yaml
 
-sleep 30
+sleep 5
 
 echo 'adding shards to mongos'
 export RS0_ENDPOINTS=$(kubectl -n dojot get endpoints mongosh0 | grep mongosh0 | awk '{print $2}')
 export RS1_ENDPOINTS=$(kubectl -n dojot get endpoints mongosh1 | grep mongosh1 | awk '{print $2}')
 export RS2_ENDPOINTS=$(kubectl -n dojot get endpoints mongosh2 | grep mongosh2 | awk '{print $2}')
-kubectl -n dojot exec mongos-0 -- mongo --eval "sh.addShard('rs0/$RS0_ENDPOINTS')"
-kubectl -n dojot exec mongos-0 -- mongo --eval "sh.addShard('rs1/$RS1_ENDPOINTS')"
-kubectl -n dojot exec mongos-0 -- mongo --eval "sh.addShard('rs2/$RS2_ENDPOINTS')"
+kubectl -n dojot exec mongodb-0 -- mongo --eval "sh.addShard('rs0/$RS0_ENDPOINTS')"
+kubectl -n dojot exec mongodb-0 -- mongo --eval "sh.addShard('rs1/$RS1_ENDPOINTS')"
+kubectl -n dojot exec mongodb-0 -- mongo --eval "sh.addShard('rs2/$RS2_ENDPOINTS')"
 
 echo 'enabling sharding'
 export SHARDJS=$(cat shard.js)
-kubectl -n dojot exec mongos-0 -- mongo --eval "$SHARDJS"
+kubectl -n dojot exec mongodb-0 -- mongo --eval "$SHARDJS"
 
 echo 'adding data'
 export ADDJS=$(cat add_data.js)
-kubectl -n dojot exec mongos-0 -- mongo --eval "$ADDJS"
+kubectl -n dojot exec mongodb-0 -- mongo --eval "$ADDJS"
