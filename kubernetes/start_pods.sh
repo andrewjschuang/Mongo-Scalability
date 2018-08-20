c89702ebb0d83b4b970ee815026cf63a406a7593
Andrew Jye Shih Chuang <achuang@cpqd.com.br>
2018-08-20 10:41:37 -0300

    Executes N shards

--- kubernetes/start_pods.sh
+++ kubernetes/start_pods.sh
@@ -1,36 +1,46 @@
-echo 'creating namespace'
+# checks if positional argument is an integer
+re='^[0-9]+$'
+if [ "$1" == "" ]; then
+    echo "defaulting to N_SHARDS=3"
+    export N_SHARDS=3
+elif ! [[ $1 =~ $re ]]; then
+    echo "insert a valid integer"
+    exit 1
+else
+    export N_SHARDS=$1
+fi
+
+echo "creating namespace"
 kubectl create namespace dojot
 
-echo 'starting 3 shard replicas'
-kubectl -n dojot apply -f mongosh0.yaml
-kubectl -n dojot apply -f mongosh1.yaml
-kubectl -n dojot apply -f mongosh2.yaml
+# creates new yaml file for each shard
+echo "starting $N_SHARDS shard replicas"
+for ((i=0;i<N_SHARDS;i++)); do
+    cp mongosh.yaml mongosh$i.yaml
+    sed -i -e "s/mongosh./mongosh$i/g" mongosh$i.yaml
+    sed -i -e "s/- rs./- rs$i/g" mongosh$i.yaml
+    kubectl -n dojot apply -f mongosh$i.yaml
+done
 
-echo 'starting config server replica'
+echo "starting config server replica"
 kubectl -n dojot apply -f mongocfg.yaml
 
-echo 'waiting for initialization to finish'
-sleep 1m
+# needs to wait enough time for all pods to be running
+echo "waiting for initialization to finish"
+sleep 2m
 
-echo 'starting mongos'
+# applies config servers endpoints to mongos.yaml file
+echo "starting mongos"
 export CONFIG_ENDPOINTS=$(kubectl -n dojot get endpoints mongocfg | grep mongocfg | awk '{print $2}')
 sed -i '/configReplSet/c\        - configReplSet/'"$CONFIG_ENDPOINTS" mongos.yaml
 kubectl -n dojot apply -f mongos.yaml
 
-sleep 10
+# needs to wait enough time for mongos pod to be running
+sleep 20
 
-echo 'adding shards to mongos'
-export RS0_ENDPOINTS=$(kubectl -n dojot get endpoints mongosh0 | grep mongosh0 | awk '{print $2}')
-export RS1_ENDPOINTS=$(kubectl -n dojot get endpoints mongosh1 | grep mongosh1 | awk '{print $2}')
-export RS2_ENDPOINTS=$(kubectl -n dojot get endpoints mongosh2 | grep mongosh2 | awk '{print $2}')
-kubectl -n dojot exec mongodb-0 -- mongo --eval "sh.addShard('rs0/$RS0_ENDPOINTS')"
-kubectl -n dojot exec mongodb-0 -- mongo --eval "sh.addShard('rs1/$RS1_ENDPOINTS')"
-kubectl -n dojot exec mongodb-0 -- mongo --eval "sh.addShard('rs2/$RS2_ENDPOINTS')"
+# adds endpoints for each shard (replica set) to mongos
+echo "adding shards to mongos"
+for ((i=0;i<N_SHARDS;i++)); do
+    kubectl -n dojot exec mongodb-0 -c mongodb -- mongo --eval "sh.addShard('rs$i/$(kubectl -n dojot get endpoints mongosh$i | grep mongosh$i | awk '{print $2}')')"
+done
 
-# echo 'enabling sharding'
-# export SHARDJS=$(cat shard.js)
-# kubectl -n dojot exec mongodb-0 -- mongo --eval "$SHARDJS"
-
-# echo 'adding data'
-# export ADDJS=$(cat add_data.js)
-# kubectl -n dojot exec mongodb-0 -- mongo --eval "$ADDJS"
